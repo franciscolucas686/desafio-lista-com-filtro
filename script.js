c9fef35deb3ee5e86a5289edf11a1bbffae29ca3
franciscolucas686 <117124430+franciscolucas686@users.noreply.github.com>
2023-06-07 20:05:35 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,53 @@
+const url = "https://dummyjson.com/users"
+
+
+function getAllUsers() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        userList = document.querySelector("#user-list")
+        userList.innerHTML = ""
+
+        data.users.forEach((user) => {
+            const divUser = document.createElement("div")
+            divUser.className = "user"
+    
+            divUser.innerHTML = `
+            <img src = "${user.image}" alt="user picture">
+            <p class ="nome">${user.firstName} ${user.maidenName} ${user.lastName}</p>
+            <span>${user.email}</span>
+            `
+    
+            userList.appendChild(divUser)   
+        });
+    })
+    .catch((error) => {
+        console.error('Erro ao obter os usuários', error)
+    })
+
+    
+}
+getAllUsers()
+
+
+const searchTerm = document.querySelector('#input-search')
+
+
+
+
+
+
+
+
+
+
+
+// searchTerm.addEventListener('keyup', ()=>{
+//     console.log(searchTerm.value.toLowerCase())
+// })
+
+
+
+
+
