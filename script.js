e19a62d75362012e46d37c90b3358b15aa6ddad5
Fernando <fermoraeslima10@gmail.com>
2023-06-15 23:23:55 -0300

    feat: finish logic

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-let users = [];
+let initialUsers = [];
+
+const SORT_ORDERS = {
+  asc: 'a-z',
+  desc: 'z-a'
+};
 
 const usersElement = document.getElementById("user-list");
-const searchElement = document.querySelector('#input-search');
+const searchElement = document.getElementById('input-search');
+const sortElement = document.getElementById('sort-field');
 
+function clearSearchValue() {
+  searchElement.value = '';
+}
 
+function clearContainerApp() {
+  renderContainerApp('');
+}
 
+function renderContainerApp(node) {
+  usersElement.innerHTML = node;
+}
 
+function renderListUsers(users) {
+  clearContainerApp();
 
-
-function renderUsers(users) {
-  usersElement.innerHTML = "";
+  if (!users?.length) {
+    renderContainerApp('<p>Nenhum usuário encontrado</p>');
+    return;
+  }
 
   users.forEach((user) => {
     const element = document.createElement("div");
@@ -25,66 +43,57 @@ function renderUsers(users) {
   });
 }
 
-async function getAllUsers() {
-    const response =  await fetch("https://dummyjson.com/users")
-    
-    const data = await response.json()
-
-    const users = data.users
-
-    renderUsers(users)
-  }
-
-function filterUser(term) {
-    
-   
-
-      
-
-        
-        
-        
-        
-        
-      
-      
-      
-  
-  
-  
-      
-  
-  
-
-  
+function getFullName(user) {
+  return `${user.firstName} ${user.maidenName} ${user.lastName}`;
 }
 
-    
-  
+async function getAllUsers() {
+  try {
+    const response =  await fetch("https://dummyjson.com/users");
+    const data = await response.json();
+    initialUsers = sortUsers(data.users, SORT_ORDERS.asc);
+    renderListUsers(initialUsers);
+  } catch (error) {
+    console.error('Não foi possível se comunicar com a API.');
+  }
+}
 
-function sortUsers(order) {}
+function filterUserByName(value) {
+  const term = value.toLowerCase();
 
-window.addEventListener("load", () => {
-  getAllUsers();
-  filterUser()
+  if (!term) {
+    return initialUsers;
+  }
 
+  return initialUsers.filter(user => getFullName(user).toLowerCase().startsWith(term));
+}
+
+function sortUsers(users, order) {
+  if (order === SORT_ORDERS.asc) {
+    return users.sort((a, b) => {
+      if (a.firstName < b.firstName) return -1;
+      if (a.firstName > b.firstName) return 1;
+      return 0;
+    });
+  }
+
+  return users.sort((a, b) => {
+    if (a.firstName > b.firstName) return -1;
+    if (a.firstName < b.firstName) return 1;
+    return 0;
+  });
+}
+
+searchElement.addEventListener("keyup", (e) => {
+  const inputValue = e.target.value;
+  const newUsers = filterUserByName(inputValue);
+  renderListUsers(newUsers);
 });
 
+sortElement.addEventListener("change", (e) => {
+  const selectValue = e.target.value;
+  const newUsers = sortUsers(initialUsers, selectValue);
+  renderListUsers(newUsers);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+window.addEventListener("load", () => getAllUsers());
